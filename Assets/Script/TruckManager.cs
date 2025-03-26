0ed0eb095c670468d4c258b8119ea3ed5ec522e6
tripti <>
2025-03-26 14:58:33 +0530

    Vehicles and Scenes

--- Assets/Script/TruckManager.cs
+++ Assets/Script/TruckManager.cs
@@ -3,30 +3,29 @@ using UnityEngine;
 public class TruckViewController : MonoBehaviour
 {
     [Header("Truck and View Positions")]
-    [SerializeField] private GameObject truck; // The truck GameObject
-    [SerializeField] private Transform rightViewCube; // Reference position for the right view
-    [SerializeField] private Transform leftViewCube;  // Reference position for the left view
-    [SerializeField] private Transform topViewCube;   // Reference position for the top view
+    [SerializeField] private GameObject truck; 
+    [SerializeField] private Transform rightViewCube; 
+    [SerializeField] private Transform leftViewCube;
+    [SerializeField] private Transform topViewCube;
 
-    // Function to show the right view
+
     public void ShowRightView()
     {
         MoveTruckToView(rightViewCube);
     }
 
-    // Function to show the left view
+ 
     public void ShowLeftView()
     {
         MoveTruckToView(leftViewCube);
     }
 
-    // Function to show the top view
     public void ShowTopView()
     {
         MoveTruckToView(topViewCube);
     }
 
-    // Helper function to move the truck to the specified view position
+   
     private void MoveTruckToView(Transform viewTransform)
     {
         truck.transform.position = viewTransform.position;
